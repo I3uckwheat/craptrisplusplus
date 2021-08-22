0919bf6e5731c8ec0cad60f26e60f236d3cfaeda
Briggs Elsperger <briggse@protonmail.com>
2021-08-22 13:41:09 -0600

    adds constants

---
+++ constants.hh
@@ -0,0 +1,7 @@
+#pragma once
+namespace constants {
+  constexpr int SCREEN_WIDTH{ 640 };
+  constexpr int SCREEN_HEIGHT{ 480 };
+  constexpr int SCREEN_FPS{ 24 };
+  constexpr int SCREEN_TICKS_PER_FRAME{ 1000 / SCREEN_FPS };
+} 
